3fe7e0effb53ef4dab49707cc50a435dba878c8b
pranayramtekkar2003 <pranayramtekkar2003@gmail.com>
2026-06-24 01:57:43 +0530

    Created the whole workflow and integrated to frontend

---
+++ src/langgraph/graph/graph_builder.py
@@ -0,0 +1,24 @@
+from langgraph.graph import StateGraph, START, END
+from src.langgraph.state.state import State
+from src.langgraph.nodes.chatbot import BasicChatbotNode
+
+class GraphBuilder:
+    def __init__(self,model):
+        self.llm = model
+        self.graph_builder = StateGraph(State)
+
+    def basic_chatbot_graph(self):
+
+        self.basic_chatbot_node = BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
+
+        self.graph_builder.add_edge(START,"chatbot")
+        self.graph_builder.add_edge("chatbot",END)
+    
+    def setup_graph(self, usecase:str):
+        
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_graph()
+        
+        return self.graph_builder.compile()
--- src/langgraph/main.py
+++ src/langgraph/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
-
+from src.langgraph.LLM.groq_llm import GroqLLM
 from src.langgraph.ui.streamlit.loadui import LoadStreamlitUI
+from src.langgraph.ui.streamlit.resultDisplay import DisplayResultStreamlit
+from src.langgraph.graph.graph_builder import GraphBuilder
 
 def load_app():
 
@@ -14,4 +16,28 @@ def load_app():
     user_message = st.chat_input("Enter your message:")
 
     if user_message:
-        pass
+        try:
+            obj_llm = GroqLLM(user_control_input=user_input)
+            model = obj_llm.get_llm_model()
+
+            if not model:
+                st.error("Error: LLM could not be initalized.")
+                return
+            
+            usecase = user_input.get("selected_usecase")
+            if not usecase:
+                st.error("Error: No usecase selected")
+                return
+            
+
+            builder = GraphBuilder(model)
+            try:
+                graph = builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase,graph,user_message).display_result()
+            except Exception as e:
+                st.error(f"Graph setup Failed: {e}")
+                return
+
+        except Exception as e:
+            st.error(f"Setup Failed: {e}")
+            return
